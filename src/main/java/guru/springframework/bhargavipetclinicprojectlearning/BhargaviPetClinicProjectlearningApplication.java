45a5ceb53e502cf4e76a602b94133b588d2994e8
bhargavibalagani <bhargavi.balagani@gmail.com>
2019-12-15 16:14:32 -0500

    Initial project add

---
+++ src/main/java/guru/springframework/bhargavipetclinicprojectlearning/BhargaviPetClinicProjectlearningApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.bhargavipetclinicprojectlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhargaviPetClinicProjectlearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BhargaviPetClinicProjectlearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/bhargavipetclinicprojectlearning/BhargaviPetClinicProjectlearningApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.bhargavipetclinicprojectlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BhargaviPetClinicProjectlearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
